2aa6d04eb58e72b2aae9f6911b117ffb192c6ec1
xanxys <xanxys@gmail.com>
2014-02-24 23:39:28 +0900

    Color clusters.

--- main.cpp
+++ main.cpp
@@ -94,35 +94,37 @@ void SimpleOpenNIViewer::cloud_cb_(const pcl::PointCloud<pcl::PointXYZ>::ConstPt
 
 	std::vector<pcl::PointIndices> cluster_indices;
 	pcl::EuclideanClusterExtraction<pcl::PointXYZ> ec;
-	ec.setClusterTolerance(0.02); // 2cm
+	ec.setClusterTolerance(0.05);
 	ec.setMinClusterSize(100);
 	ec.setMaxClusterSize(25000);
 	ec.setSearchMethod(tree);
 	ec.setInputCloud(cloud_filtered);
 	ec.extract(cluster_indices);
 
-	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_cluster(new pcl::PointCloud<pcl::PointXYZ>);
-	int j = 0;
-	for(std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin(); it != cluster_indices.end(); ++it){
-	
-		
-		for(std::vector<int>::const_iterator pit = it->indices.begin(); pit != it->indices.end(); pit++) {
-			
-			cloud_cluster->points.push_back(cloud_filtered->points[*pit]); //*
-		}
-		cloud_cluster->width = cloud_cluster->points.size();
-		cloud_cluster->height = 1;
-		cloud_cluster->is_dense = true;
+	// Pack colored points.
+	pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud_cluster(
+		new pcl::PointCloud<pcl::PointXYZRGBA>());
 
-		std::cout << "PointCloud representing the Cluster: " << cloud_cluster->points.size() << " data points." << std::endl;
-		/*
-		std::stringstream ss;
-		ss << "cloud_cluster_" << j << ".pcd";
-		writer.write<pcl::PointXYZ>(ss.str(), *cloud_cluster, false); //*
-		*/
-		j++;
-		break;
+	int cluster_ix = 0;
+	cloud_cluster->width = 0;
+	for(const auto& point_indices : cluster_indices) {
+		for(int index : point_indices.indices) {
+			const pcl::PointXYZ& pt_xyz = cloud_filtered->points[index];
+
+			pcl::PointXYZRGBA pt;
+			pt.getVector3fMap() = pt_xyz.getVector3fMap();
+			pt.r = cluster_ix * 10;
+			pt.g = 0xff;
+			pt.b = 255 - pt.r;
+			pt.a = 0xff;
+
+			cloud_cluster->points.push_back(pt);
+		}
+		cloud_cluster->width += cloud_cluster->points.size();
+		cluster_ix++;
 	}
+	cloud_cluster->height = 1;
+	cloud_cluster->is_dense = true;
 
 	viewer.showCloud(cloud_cluster);
 }
