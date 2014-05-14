f50210b2e1f20d0361b553c4bf8a3fdc35b6cbef
xanxys <xanxys@gmail.com>
2014-05-14 22:18:34 +0900

    Implement 1-core L1 norm collection job.

--- job_pool.h
+++ job_pool.h
@@ -1,11 +1,39 @@
 #pragma once
 
 #include <string>
+#include <thread>
 #include <vector>
 
+#include <boost/optional.hpp>
 #include <jsoncpp/json/json.h>
 
 #include "data_source.h"
+
+class EstimateL1Job {
+public:
+	// Existence of Json::Value somehow prevents default move constructor
+	// generation, so we need manual move constructor.
+	EstimateL1Job(EstimateL1Job&& that);
+	EstimateL1Job(std::string id, DataSource& data_source, std::vector<std::string> scene_ids);
+
+	float getProgress();
+	boost::optional<Json::Value> getResult();
+private:
+	// Calculate L1 norm for all scene_ids and aggreate result as array.
+	void estimateL1(std::vector<std::string> scene_ids);
+
+	// assuming shared, high-bandwidth DataSource, scene_id is enough.
+	Json::Value mapEstimateL1(std::string scene_id);
+public:
+	const std::string id;
+private:
+	DataSource& data_source;
+	std::thread worker;
+
+	// TODO: thread-unsafe
+	Json::Value result;
+};
+
 
 // JobPool initiates, maintains, possibly kills jobs.
 // Typically jobs depends on filtered version of DataSource.
@@ -21,9 +49,9 @@ public:
 
 	std::string createJob();
 private:
-	// Borrowed. (TODO: maybe ideally it should be consr?)
+	// Borrowed. (TODO: maybe ideally it should be const?)
 	DataSource& data_source;
 
-	std::vector<std::string> all_jobs;
+	std::vector<EstimateL1Job> all_jobs;
 	int job_id;
 };
