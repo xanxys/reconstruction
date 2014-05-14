db92c41d49578f758b366b2bb203b472e7073168
xanxys <xanxys@gmail.com>
2014-05-14 22:40:49 +0900

    Make UI clearer

--- http_static/panel_job.js
+++ http_static/panel_job.js
@@ -12,6 +12,12 @@ var JobPanel = Backbone.View.extend({
 	render: function() {
 		$('#panel').html(_.template($('script[name=job_panel]').html()));
 
-		$('#job_summary').text(JSON.stringify(this.model.attributes));
+		$('#job_summary').empty();
+		$('#job_summary').append($('<h1/>').text('Job ' + this.model.id));
+		$('#job_summary').append($('<h2/>').text('Raw Result'));
+		$('#job_summary').append($('<pre/>')
+			.text(JSON.stringify(this.model.attributes, undefined, '  '))
+			.css('height', '400px')
+			.css('overflow', 'scroll'));
 	}
 });
