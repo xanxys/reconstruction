e3975a0f3926a9a08457b451542295cab7dccace
xanxys <xanxys@gmail.com>
2014-05-24 13:43:28 +0900

    List all RGB frames

--- master.py
+++ master.py
@@ -2,11 +2,30 @@
 from __future__ import print_function, division
 import flask
 
+import os
+import cv2
+
 app = flask.Flask(__name__)
+
+dataset_root = '/data-new/research/2014/reconstruction/NYU2-slice'
 
 @app.route('/')
 def hello():
-	return 'Hello World!'
+	ls = ''
+	for d in os.listdir(dataset_root):
+		ls += '<img src="/%s">' % d
+	return 'List of Images' + ls
+
+@app.route('/<scene_id>')
+def image(scene_id):
+	scene_root = os.path.join(dataset_root, scene_id)
+	[rgb] = [f for f in os.listdir(scene_root) if f.endswith('.ppm')]
+	rgb_image = cv2.imread(os.path.join(scene_root, rgb))
+
+	binary = cv2.imencode('.jpg', rgb_image)[1].tostring()
+	resp = flask.make_response(binary)
+	resp.headers['Content-Type'] = 'image/jpeg'
+	return resp
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
