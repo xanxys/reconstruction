3d4e8b9de2eb417a84e858ccc72d8b5ff0414759
xanxys <xanxys@gmail.com>
2014-05-26 12:16:09 +0900

    Init python 3D notebook.

--- master.py
+++ master.py
@@ -2,19 +2,23 @@
 from __future__ import print_function, division
 import flask
 
+import imp
 import os
 import cv2
 
-app = flask.Flask(__name__)
+slic = imp.load_dynamic('slic', 'SLIC-Superpixels/slic.so')
+
+app = flask.Flask(__name__, static_url_path='/py_static')
 
 dataset_root = '/data-new/research/2014/reconstruction/NYU2-slice'
 
 @app.route('/')
-def hello():
-	ls = ''
-	for d in os.listdir(dataset_root):
-		ls += '<img src="/%s">' % d
-	return 'List of Images' + ls
+def index():
+	return flask.send_from_directory('./py_static', 'index.html')
+
+@app.route('/static/<path:path>')
+def serve_static(path):
+	return flask.send_from_directory('./static', path)
 
 @app.route('/<scene_id>')
 def image(scene_id):
@@ -22,10 +26,12 @@ def image(scene_id):
 	[rgb] = [f for f in os.listdir(scene_root) if f.endswith('.ppm')]
 	rgb_image = cv2.imread(os.path.join(scene_root, rgb))
 
+	rgb_image = slic.oversegmentate(rgb_image)
+
 	binary = cv2.imencode('.jpg', rgb_image)[1].tostring()
 	resp = flask.make_response(binary)
 	resp.headers['Content-Type'] = 'image/jpeg'
 	return resp
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(debug=True, port=5321)
