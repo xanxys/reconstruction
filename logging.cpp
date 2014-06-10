9505ee6b2163d0d57f26eb6e7c0976a5290c3191
xanxys <xanxys@gmail.com>
2014-06-10 15:03:09 +0900

    Add DEBUG-level logging.

---
+++ logging.cpp
@@ -0,0 +1,11 @@
+#include "logging.h"
+
+void logJsonRaw(std::string level, std::string path, int line, Json::Value& msg) {
+	Json::Value entry;
+	entry["msg"] = msg;
+	entry["date"] = boost::posix_time::to_iso_extended_string(boost::posix_time::microsec_clock::universal_time());
+	entry["level"] = level;
+	entry["loc"]["path"] = path;
+	entry["loc"]["line"] = line;
+	std::cout << Json::FastWriter().write(entry);
+}
--- logging.h
+++ logging.h
@@ -7,57 +7,42 @@
 
 
 #define INFO(...) logJson("INFO", __FILE__, __LINE__, __VA_ARGS__)
+#define DEBUG(...) logJson("DEBUG", __FILE__, __LINE__, __VA_ARGS__)
+
+void logJsonRaw(std::string level, std::string path, int line, Json::Value& msg);
 
 template<typename MessageType>
 void logJson(std::string level, std::string path, int line, MessageType msg) {
-	Json::Value entry;
-	entry["msg"] = msg;
-	entry["date"] = boost::posix_time::to_iso_extended_string(boost::posix_time::microsec_clock::universal_time());
-	entry["level"] = level;
-	entry["loc"]["path"] = path;
-	entry["loc"]["line"] = line;
-	std::cout << Json::FastWriter().write(entry);
+	Json::Value message = msg;
+	logJsonRaw(level, path, line, message);
 }
-
 
 template<typename MessageType1, typename MessageType2>
 void logJson(std::string level, std::string path, int line,
 	MessageType1 msg1, MessageType2 msg2) {
-	Json::Value entry;
-	entry["msg"].append(msg1);
-	entry["msg"].append(msg2);
-	entry["date"] = boost::posix_time::to_iso_extended_string(boost::posix_time::microsec_clock::universal_time());
-	entry["level"] = level;
-	entry["loc"]["path"] = path;
-	entry["loc"]["line"] = line;
-	std::cout << Json::FastWriter().write(entry);
+	Json::Value message;
+	message.append(msg1);
+	message.append(msg2);
+	logJsonRaw(level, path, line, message);
 }
 
 template<typename MessageType1, typename MessageType2, typename MessageType3>
 void logJson(std::string level, std::string path, int line,
 	MessageType1 msg1, MessageType2 msg2, MessageType3 msg3) {
-	Json::Value entry;
-	entry["msg"].append(msg1);
-	entry["msg"].append(msg2);
-	entry["msg"].append(msg3);
-	entry["date"] = boost::posix_time::to_iso_extended_string(boost::posix_time::microsec_clock::universal_time());
-	entry["level"] = level;
-	entry["loc"]["path"] = path;
-	entry["loc"]["line"] = line;
-	std::cout << Json::FastWriter().write(entry);
+	Json::Value message;
+	message.append(msg1);
+	message.append(msg2);
+	message.append(msg3);
+	logJsonRaw(level, path, line, message);
 }
 
 template<typename MessageType1, typename MessageType2, typename MessageType3, typename MessageType4>
 void logJson(std::string level, std::string path, int line,
 	MessageType1 msg1, MessageType2 msg2, MessageType3 msg3, MessageType4 msg4) {
-	Json::Value entry;
-	entry["msg"].append(msg1);
-	entry["msg"].append(msg2);
-	entry["msg"].append(msg3);
-	entry["msg"].append(msg4);
-	entry["date"] = boost::posix_time::to_iso_extended_string(boost::posix_time::microsec_clock::universal_time());
-	entry["level"] = level;
-	entry["loc"]["path"] = path;
-	entry["loc"]["line"] = line;
-	std::cout << Json::FastWriter().write(entry);
+	Json::Value message;
+	message.append(msg1);
+	message.append(msg2);
+	message.append(msg3);
+	message.append(msg4);
+	logJsonRaw(level, path, line, message);
 }
