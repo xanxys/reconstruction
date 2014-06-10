ec486e5d17f271be3ae42eebe45d293a79bb5b04
xanxys <xanxys@gmail.com>
2014-06-10 17:10:27 +0900

    Add point cloud -> mesh baking.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <array>
 #include <iostream>
 #include <fstream>
+#include <limits>
 
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
@@ -41,6 +42,53 @@ void testMeshIO() {
 	writeObjMaterial(test_mat);
 }
 
+// Get barycentric coordinate of the narest point on triangle surface.
+Eigen::Vector2f nearestBarycentricApprox(
+	const Eigen::Vector3f& v0, const Eigen::Vector3f& v1, const Eigen::Vector3f& v2, const Eigen::Vector3f& p) {
+	const auto d1 = v1 - v0;
+	const auto d2 = v2 - v0;
+	Eigen::Matrix3f m;
+	m.col(0) = d1;
+	m.col(1) = d2;
+	m.col(2) = d1.cross(d2);
+
+	const Eigen::Vector3f coord = m.inverse() * (p - v0);
+	// approximate barycentric coordinates.
+	float bary0 = std::max(0.0f, coord(0));
+	float bary1 = std::max(0.0f, coord(1));
+	if(bary1 + bary0 > 1) {
+		const float scale = 1 / (bary0 + bary1);
+		bary0 *= scale;
+		bary1 *= scale;
+	}
+	return Eigen::Vector2f(bary0, bary1);
+}
+
+// Get linear-interpolate coordinate of the surfact point, which is nearest to the given point.
+Eigen::Vector2f nearestCoordinate(
+	const TriangleMesh<Eigen::Vector2f>& mesh, const Eigen::Vector3f p) {
+	float min_dist = std::numeric_limits<float>::max();
+	Eigen::Vector2f coord;
+	for(const auto& tri : mesh.triangles) {
+		const Eigen::Vector3f v0 = mesh.vertices[std::get<0>(tri)].first;
+		const Eigen::Vector3f v1 = mesh.vertices[std::get<1>(tri)].first;
+		const Eigen::Vector3f v2 = mesh.vertices[std::get<2>(tri)].first;
+		const auto bary = nearestBarycentricApprox(v0, v1, v2, p);
+
+		const Eigen::Vector3f pt_nearest = v0 + (v1 - v0) * bary(0) + (v2 - v0) * bary(1);
+		const float dist = (p - pt_nearest).norm();
+		if(dist < min_dist) {
+			min_dist = dist;
+
+			const Eigen::Vector2f c0 = mesh.vertices[std::get<0>(tri)].second;
+			const Eigen::Vector2f c1 = mesh.vertices[std::get<1>(tri)].second;
+			const Eigen::Vector2f c2 = mesh.vertices[std::get<2>(tri)].second;
+			coord = c0 + (c1 - c0) * bary(0) + (c2 - c0) * bary(1);
+		}
+	}
+	return coord;
+}
+
 void testPointCloudMeshing() {
 	INFO("Creating textured mesh from point cloud");
 	std::ifstream test("ocha_points.json");
@@ -62,6 +110,29 @@ void testPointCloudMeshing() {
 	mesh_uv.serializeObjWithUv(room_box_uv, "uv_debug.mtl");
 
 	cv::imwrite("uv_box.png", visualizeUVMap(mesh_uv));
+
+	// Project points to the surface and draw circle onto texture.
+	const int tex_size = 1024;
+	cv::Mat texture(tex_size, tex_size, CV_8UC3);
+	texture = cv::Scalar(0, 0, 0);
+	for(const auto& point : cloud) {
+		const Eigen::Vector3f pos(
+			point["x"].asDouble(),
+			point["y"].asDouble(),
+			point["z"].asDouble());
+		const auto uv = nearestCoordinate(mesh_uv, pos);
+		// DEBUG("nearestUV", uv(0), uv(1));
+
+		const cv::Scalar color(
+			point["b"].asDouble(),
+			point["g"].asDouble(),
+			point["r"].asDouble());
+
+		cv::circle(
+			texture, eigenToCV(swapY(uv) * tex_size), 3,
+			color, -1);
+	}
+	cv::imwrite("uv_pt_baked.png", texture);
 }
 
 int main() {
