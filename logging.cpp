c5afb07f8c176e134c19233bd2b0bc6cddb6de69
xanxys <xanxys@gmail.com>
2014-09-07 10:13:32 +0900

    Add session id to logs.

--- logging.cpp
+++ logging.cpp
@@ -1,7 +1,18 @@
 #include "logging.h"
 
+#include <limits>
+#include <random>
+
 void logJsonRaw(std::string level, std::string path, int line, Json::Value& msg) {
+	static std::string session_id = "";
+	if(session_id == "") {
+		std::random_device rd;
+		using SessionId = uint64_t;
+		const SessionId v = std::uniform_int_distribution<SessionId>(0, std::numeric_limits<SessionId>::max())(rd);
+		session_id = std::to_string(v);
+	}
 	Json::Value entry;
+	entry["session_id"] = session_id;
 	entry["msg"] = msg;
 	entry["date"] = boost::posix_time::to_iso_extended_string(boost::posix_time::microsec_clock::universal_time());
 	entry["level"] = level;
