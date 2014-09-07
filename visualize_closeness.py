29eac98e3932cd2aa041e27236cbc5426ff12e64
xanxys <xanxys@gmail.com>
2014-09-07 11:29:56 +0900

    Session id based hierarchical visualization.

--- visualize_closeness.py
+++ visualize_closeness.py
@@ -1,16 +1,35 @@
 #!/bin/python2
 # coding: utf-8
+from __future__ import print_function, division
 import matplotlib.pyplot as plt
 import numpy as np
 import json
+import networkx as nx
+
+
+def get_sessions(fobj):
+    """
+    return {(session_id, session logs)}
+    Entries with no session id will have "" as id.
+    """
+    log = [json.loads(line) for line in fobj]
+    sessions = {}
+    for entry in log:
+        sid = entry.get("session_id", "")
+        sessions.setdefault(sid, []).append(entry)
+    return sessions
 
 if __name__ == '__main__':
-    log = [json.loads(line) for line in open('log')]
+    for (sid, session) in get_sessions(open("log")).items():
+        print('Processing sid=%s at %s' % (sid, session[0]["date"]))
+        if sid == "":
+            print('WARNING: old logs with empty sid found. Some results might be messed up.')
 
-    targets = filter(lambda e: e["msg"][0] == "closeness matrix", log)
+        plt.figure(1)
 
-    for target in targets:
-        print('Processing %s' % target["date"])
+        # closeness matrix
+        plt.subplot(121)
+        target = filter(lambda e: e["msg"][0] == "closeness matrix", session)[0]
         closeness = target["msg"][1]
         closeness = np.array(closeness)
 
@@ -19,4 +38,17 @@ if __name__ == '__main__':
         plt.xlabel('scan index')
         plt.ylabel('scan index')
         plt.colorbar(x)
+
+        # Pairings
+        plt.subplot(122)
+        pairs = filter(lambda e: e["msg"][0] == "Pair:", session)
+        pairs = [(pair["msg"][1], pair["msg"][2], pair["msg"][4]) for pair in pairs]
+        g = nx.DiGraph()
+        for (source0, source1, target) in pairs:
+            g.add_edge(source0, target)
+            g.add_edge(source1, target)
+        p = nx.graphviz_layout(g, prog='twopi', args='')
+        nx.draw(g, p, node_color='skyblue', alpha=0.8)
+
+        # show all
         plt.show()
