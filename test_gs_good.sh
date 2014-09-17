a34dfa274d4b76cffe7216fbf27b5a1cc207420c
xanxys <xanxys@gmail.com>
2014-09-17 15:35:01 +0900

    Reverting to use all 15 scans since they now seem to align within calibration error.

--- test_gs_good.sh
+++ test_gs_good.sh
@@ -16,6 +16,24 @@
 # scan-20140827-13:53-gakusei-small
 # scan-20140827-13:57-gakusei-small
 
+# All 15
+time ./recon --convert \
+	../capturer/scan-20140827-12:57-gakusei-small \
+	../capturer/scan-20140827-13:05-gakusei-small \
+	../capturer/scan-20140827-13:11-gakusei-small \
+	../capturer/scan-20140827-13:15-gakusei-small \
+	../capturer/scan-20140827-13:18-gakusei-small \
+	../capturer/scan-20140827-13:23-gakusei-small \
+	../capturer/scan-20140827-13:26-gakusei-small \
+	../capturer/scan-20140827-13:31-gakusei-small \
+	../capturer/scan-20140827-13:34-gakusei-small \
+	../capturer/scan-20140827-13:38-gakusei-small \
+	../capturer/scan-20140827-13:42-gakusei-small \
+	../capturer/scan-20140827-13:45-gakusei-small \
+	../capturer/scan-20140827-13:49-gakusei-small \
+	../capturer/scan-20140827-13:53-gakusei-small \
+	../capturer/scan-20140827-13:57-gakusei-small
+
 # time ./recon --convert \
 # 	../capturer/scan-20140827-13:23-gakusei-small \
 # 	../capturer/scan-20140827-13:26-gakusei-small
@@ -45,15 +63,15 @@
 # ../capturer/scan-20140827-13:57-gakusei-small
 
 # merged good parts
-time ./recon --convert \
-	../capturer/scan-20140827-12:57-gakusei-small \
-	../capturer/scan-20140827-13:05-gakusei-small \
-	../capturer/scan-20140827-13:11-gakusei-small \
-	../capturer/scan-20140827-13:15-gakusei-small \
-	../capturer/scan-20140827-13:18-gakusei-small \
-	../capturer/scan-20140827-13:23-gakusei-small \
-	../capturer/scan-20140827-13:26-gakusei-small \
-	../capturer/scan-20140827-13:26-gakusei-small \
-	../capturer/scan-20140827-13:31-gakusei-small \
-	../capturer/scan-20140827-13:34-gakusei-small \
-	../capturer/scan-20140827-13:45-gakusei-small
+# time ./recon --convert \
+# 	../capturer/scan-20140827-12:57-gakusei-small \
+# 	../capturer/scan-20140827-13:05-gakusei-small \
+# 	../capturer/scan-20140827-13:11-gakusei-small \
+# 	../capturer/scan-20140827-13:15-gakusei-small \
+# 	../capturer/scan-20140827-13:18-gakusei-small \
+# 	../capturer/scan-20140827-13:23-gakusei-small \
+# 	../capturer/scan-20140827-13:26-gakusei-small \
+# 	../capturer/scan-20140827-13:26-gakusei-small \
+# 	../capturer/scan-20140827-13:31-gakusei-small \
+# 	../capturer/scan-20140827-13:34-gakusei-small \
+# 	../capturer/scan-20140827-13:45-gakusei-small
