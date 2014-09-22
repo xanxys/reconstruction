bb7868e673f66884ee04593fa18b08862c72ee00
xanxys <xanxys@gmail.com>
2014-09-23 00:03:34 +0900

    Remove one of two errorneous scans from test script.

--- test_gs_good.sh
+++ test_gs_good.sh
@@ -16,14 +16,18 @@
 # scan-20140827-13:53-gakusei-small
 # scan-20140827-13:57-gakusei-small
 
-# All 15
+
+# cardbox tower error
+#../capturer/scan-20140827-13:23-gakusei-small \
+
+
+# first half
 time ./recon --convert \
 	../capturer/scan-20140827-12:57-gakusei-small \
 	../capturer/scan-20140827-13:05-gakusei-small \
 	../capturer/scan-20140827-13:11-gakusei-small \
 	../capturer/scan-20140827-13:15-gakusei-small \
 	../capturer/scan-20140827-13:18-gakusei-small \
-	../capturer/scan-20140827-13:23-gakusei-small \
 	../capturer/scan-20140827-13:26-gakusei-small \
 	../capturer/scan-20140827-13:31-gakusei-small \
 	../capturer/scan-20140827-13:34-gakusei-small \
@@ -34,44 +38,14 @@ time ./recon --convert \
 	../capturer/scan-20140827-13:53-gakusei-small \
 	../capturer/scan-20140827-13:57-gakusei-small
 
-# time ./recon --convert \
-# 	../capturer/scan-20140827-13:23-gakusei-small \
-# 	../capturer/scan-20140827-13:26-gakusei-small
-
-# first half: mostly ok
-# time ./recon --convert \
-# 	../capturer/scan-20140827-12:57-gakusei-small \
-# 	../capturer/scan-20140827-13:05-gakusei-small \
-# 	../capturer/scan-20140827-13:11-gakusei-small \
-# 	../capturer/scan-20140827-13:15-gakusei-small \
-# 	../capturer/scan-20140827-13:18-gakusei-small \
-# 	../capturer/scan-20140827-13:23-gakusei-small \
-# 	../capturer/scan-20140827-13:26-gakusei-small
-
-# second half:
-# time ./recon --convert \
-# 	../capturer/scan-20140827-13:26-gakusei-small \
-# 	../capturer/scan-20140827-13:31-gakusei-small \
-# 	../capturer/scan-20140827-13:34-gakusei-small \
-# 	../capturer/scan-20140827-13:45-gakusei-small
-
-# bad in 2nd half
-# ../capturer/scan-20140827-13:38-gakusei-small
-# ../capturer/scan-20140827-13:42-gakusei-small
-# ../capturer/scan-20140827-13:49-gakusei-small
-# ../capturer/scan-20140827-13:53-gakusei-small
+# Unfortunately, it seems that 13:26 (current target)
+# doesn't align well with ALL OTHER scan in
+# ../capturer/scan-20140827-13:31-gakusei-small \
+# ../capturer/scan-20140827-13:34-gakusei-small \
+# ../capturer/scan-20140827-13:38-gakusei-small \
+# ../capturer/scan-20140827-13:42-gakusei-small \
+# ../capturer/scan-20140827-13:45-gakusei-small \
+# ../capturer/scan-20140827-13:49-gakusei-small \
+# ../capturer/scan-20140827-13:53-gakusei-small \
 # ../capturer/scan-20140827-13:57-gakusei-small
-
-# merged good parts
-# time ./recon --convert \
-# 	../capturer/scan-20140827-12:57-gakusei-small \
-# 	../capturer/scan-20140827-13:05-gakusei-small \
-# 	../capturer/scan-20140827-13:11-gakusei-small \
-# 	../capturer/scan-20140827-13:15-gakusei-small \
-# 	../capturer/scan-20140827-13:18-gakusei-small \
-# 	../capturer/scan-20140827-13:23-gakusei-small \
-# 	../capturer/scan-20140827-13:26-gakusei-small \
-# 	../capturer/scan-20140827-13:26-gakusei-small \
-# 	../capturer/scan-20140827-13:31-gakusei-small \
-# 	../capturer/scan-20140827-13:34-gakusei-small \
-# 	../capturer/scan-20140827-13:45-gakusei-small
+# Presumably, there's very little overlap in these pairs.
