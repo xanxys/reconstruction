0740dc38d73159d3bf4b9f9efd8595c374e2963f
xanxys <xanxys@gmail.com>
2014-11-12 13:36:14 +0900

    Move program proxy intermediate files under /tmp

--- program_proxy.cpp
+++ program_proxy.cpp
@@ -13,15 +13,14 @@ Json::Value call_external(const std::string& prog_path, Json::Value arg) {
 	std::random_device rd;
 	std::uniform_int_distribution<uint32_t> dist(0, 0xffffffff);
 	const std::string id = std::to_string(dist(rd));
-	const std::string path_in = "tmp-prog_proxy-in-" + id;
-	const std::string path_out = "tmp-prog_proxy-out" + id;
+	const std::string path_in = "/tmp/recon-prog_proxy-in-" + id;
+	const std::string path_out = "/tmp/recon-prog_proxy-out" + id;
 
 	std::ofstream of(path_in);
 	of << Json::FastWriter().write(arg) << std::endl;
 	of.close();
 
 	const int status = system(("cat " + path_in + " | " + prog_path + " > " + path_out).c_str());
-	//boost::filesystem::remove(boost::filesystem::path(path_in));
 	if(status != 0) {
 		throw std::runtime_error("call_external: system() failed with " + std::to_string(status));
 	}
@@ -29,6 +28,5 @@ Json::Value call_external(const std::string& prog_path, Json::Value arg) {
 	std::ifstream in_f(path_out);
 	Json::Value result;
 	Json::Reader().parse(in_f, result, false);
-	//boost::filesystem::remove(boost::filesystem::path(path_out));
 	return result;
 }
