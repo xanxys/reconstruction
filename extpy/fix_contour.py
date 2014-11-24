694de94da183d4a378a0a859267de35820c66a3f
xanxys <xanxys@gmail.com>
2014-11-24 23:04:58 +0900

    Optimal tick selection based on AIC.

--- extpy/fix_contour.py
+++ extpy/fix_contour.py
@@ -3,6 +3,7 @@ from __future__ import print_function, division
 import cairo
 import numpy as np
 import scipy.linalg as la
+import scipy.cluster.vq
 import json
 import sys
 import math
@@ -20,7 +21,38 @@ def rotation(angle):
         [s, c]])
 
 
+def best_kmeans(vs, sigma):
+    """
+    Do k-means and return best clustering based on AIC.
+    constraint: 2 <= #cluster < 10
+    Assumes vs is i.i.d and vs ~ N(sigma)
+    """
+    smallest = (1e10, None)
+    # ln L = -ln(sigma) - 0.5*ln(2 * pi) - error ^ 2 / (2 * sigma^2)
+    for n_cluster in range(2, 10):
+        ticks, errors = scipy.cluster.vq.kmeans(vs, n_cluster)
+        likelihood = np.sum(
+            - math.log(sigma) - 0.5 * math.log(2 * math.pi)
+            - errors ** 2 / (2 * sigma ** 2))
+        aic = -2 * likelihood + 2 * n_cluster
+        # print("AIC(K=%d): %f" % (n_cluster, aic))
+        curr = (aic, ticks)
+        if curr < smallest:
+            smallest = curr
+    assert(smallest[1] is not None)
+    return smallest[1]
+
+
 class ContourAnalyzer(object):
+    """
+    Hallucinate "good" room contour from given 2-d points,
+    mainly by enforcing Manhattan assumption.
+
+    A good contour lies mostly on given points,
+    but simplicity is more important than numerical precision.
+
+    Error should be handled as features (a la CAD).
+    """
     def __init__(self, points=None, vis_path=None):
         self.points = np.array(points)
         assert(self.points.shape[1] == 2)
@@ -32,6 +64,32 @@ class ContourAnalyzer(object):
 
         self.calc_normals()
         self.choose_principle()
+        self.compress()
+
+    def compress(self):
+        """
+        Create lowest entropy description of points
+        without too much discrepancy.
+
+        Good measure is DoF of real-valued variables.
+
+        We're assuming target indoor is of a single room.
+        Thus, we can discretize each principle axis separately.
+        """
+        # Choose "ticks" of each axis.
+        # Almost everything will be quantized afterwards.
+        sigma_dist = 0.05
+        cos_thresh = math.cos(10 / 180 * math.pi)
+        vs0 = []
+        vs1 = []
+        for (p, n) in self.get_surfels():
+            if abs(np.dot(self.prin0, n)) >= cos_thresh:
+                vs0.append(np.dot(self.prin0, p))
+            elif abs(np.dot(self.prin1, n)) >= cos_thresh:
+                vs1.append(np.dot(self.prin1, p))
+        self.ticks0 = best_kmeans(np.array(vs0), sigma_dist)
+        self.ticks1 = best_kmeans(np.array(vs1), sigma_dist)
+
 
     def calc_normals(self):
         edge_to_normal = rotation(math.pi / 2)
@@ -42,6 +100,19 @@ class ContourAnalyzer(object):
             n /= la.norm(n)
             self.normals.append(n)
         assert(np.isfinite(self.normals).all())
+
+    def get_surfels(self):
+        """
+        Return [(edge midpoint, edge normal)]
+        """
+        edge_to_normal = rotation(math.pi / 2)
+        ens = []
+        for (i, p0) in enumerate(self.points):
+            p1 = self.points[(i + 1) % len(self.points)]
+            n = np.dot(edge_to_normal, p1 - p0)
+            n /= la.norm(n)
+            ens.append(((p0 + p1) / 2, n))
+        return ens
 
     def choose_principle(self):
         """
@@ -75,12 +146,33 @@ class ContourAnalyzer(object):
 
         self.draw_contour(ctx)
 
+        self.draw_ticks(ctx)
+
         if self.vis_path is not None:
             surf.write_to_png(str(self.vis_path))
 
         return {
             "vp": self.vis_path
         }
+
+    def draw_ticks(self, ctx):
+        ctx.set_line_width(0.01)
+        ctx.new_path()
+        ctx.set_source_rgba(1, 0, 0, 0.3)
+        for v0 in self.ticks0:
+            p0 = self.prin0 * v0 - self.prin1 * 10
+            p1 = self.prin0 * v0 + self.prin1 * 10
+            ctx.move_to(*p0)
+            ctx.line_to(*p1)
+        ctx.stroke()
+        ctx.new_path()
+        ctx.set_source_rgba(0, 1, 0, 0.3)
+        for v1 in self.ticks1:
+            p0 = self.prin1 * v1 - self.prin0 * 10
+            p1 = self.prin1 * v1 + self.prin0 * 10
+            ctx.move_to(*p0)
+            ctx.line_to(*p1)
+        ctx.stroke()
 
     def draw_contour(self, ctx):
         # edges
