a132069739308517d65aced6767281f14882b14b
xanxys <xanxys@gmail.com>
2014-12-11 21:51:42 +0900

    First sane collision-enabled box in NoisyActor

--- EqExperiment/Source/EqExperiment/NoisyActor.cpp
+++ EqExperiment/Source/EqExperiment/NoisyActor.cpp
@@ -7,7 +7,36 @@
 ANoisyActor::ANoisyActor(const class FPostConstructInitializeProperties& PCIP)
 	: Super(PCIP)
 {
+	const float uu_per_meter = 100;
+
+	StaticMeshComponent = PCIP.CreateAbstractDefaultSubobject<UStaticMeshComponent>(this, TEXT("staticmesh"));
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> StaticMesh(TEXT("StaticMesh'/Game/Meshes/TemplateCube_Rounded.TemplateCube_Rounded'"));
+	static ConstructorHelpers::FObjectFinder<UMaterial> Material(TEXT("MaterialInstanceConstant'/Game/Materials/M_Basic_Floor.M_Basic_Floor'"));
+	StaticMeshComponent->SetStaticMesh(StaticMesh.Object);
+	StaticMeshComponent->SetMaterial(0, Material.Object);
+	StaticMeshComponent->SetMobility(EComponentMobility::Movable);
+		
+	/*
+	BoxComponent = PCIP.CreateAbstractDefaultSubobject<UBoxComponent>(this, TEXT("box"));
+	BoxComponent->InitBoxExtent(FVector(100, 100, 100));
+	StaticMeshComponent->AttachTo(BoxComponent);
+	*/
+
+	StaticMeshComponent->SetMobility(EComponentMobility::Movable);
+	StaticMeshComponent->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	StaticMeshComponent->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Block);
+	
+		
+	PrimaryActorTick.bCanEverTick = true;
+
+	StaticMeshComponent->SetWorldLocation(FVector(100, 100, 1000));
+	StaticMeshComponent->SetPhysicsLinearVelocity(FVector(0, 0, 0));
+	
 
 }
 
-
+void ANoisyActor::BeginPlay() {
+	Super::BeginPlay();
+	StaticMeshComponent->SetSimulatePhysics(true);
+	StaticMeshComponent->WakeRigidBody();
+}
--- EqExperiment/Source/EqExperiment/NoisyActor.h
+++ EqExperiment/Source/EqExperiment/NoisyActor.h
@@ -6,13 +6,18 @@
 #include "NoisyActor.generated.h"
 
 /**
- * 
+ * ANoisyActor
+ *    |-BoxComponent (for physics, invisible)
+           |-StaticMeshComponent (for rendering, visible, no-physics)
+
  */
 UCLASS()
 class EQEXPERIMENT_API ANoisyActor : public AActor
 {
 	GENERATED_UCLASS_BODY()
 
-	
-	
+	virtual void BeginPlay() override;
+
+	TSubobjectPtr<UStaticMeshComponent> StaticMeshComponent;
+	//TSubobjectPtr<UBoxComponent> BoxComponent;
 };
