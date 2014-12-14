ec82c575d708a2f4e61fad08032ab9ea27c333ec
xanxys <xanxys@gmail.com>
2014-12-14 15:56:40 +0900

    Implement collision sound splitter.

--- sound/simulate_collision.py
+++ sound/simulate_collision.py
@@ -8,6 +8,7 @@ import wave
 import os
 import os.path
 import random
+import itertools
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -93,6 +94,93 @@ def read_wave_asset(path, freq=44100):
     return samples
 
 
+def split_collisions(sound, freq=44100, window_duration=0.01):
+    """
+    sound: [-1, 1] float array sound
+    result: array of normalized, individual collisions
+
+    input sound must be sufficiently high quality (low noise when silent)
+    for split_collisions to work.
+    """
+    n_window = int(window_duration * freq)
+    assert(len(sound) > n_window)
+
+    thresh_peak = 0.5
+    thresh_cut_b = 0.05
+    thresh_cut_e = 0.01
+    margin_beginning = 0.005  # 5msec
+
+    # find peak regions.
+    sound = sound / np.max(np.abs(sound))
+    peaks = np.convolve(
+        np.abs(sound) > thresh_peak,
+        np.ones([n_window]) / n_window) > 0
+    peak_regions = []
+    for (key, ixs) in itertools.groupby(enumerate(peaks), key=lambda x: x[1]):
+        if not key:
+            continue
+        ix_begin = next(ixs)[0]
+        ix_end = max(ixs)[0]
+        if ix_begin < ix_end:
+            peak_regions.append((ix_begin, ix_end))
+    # extend regions forward and backward until sound level is too low.
+    exist_b = np.convolve(
+        np.abs(sound) > thresh_cut_b,
+        np.ones([n_window]) / n_window) > 0
+    exist_e = np.convolve(
+        np.abs(sound) > thresh_cut_e,
+        np.ones([n_window]) / n_window) > 0
+    regions = []
+    for (ix_peak_begin, ix_peak_end) in peak_regions:
+        ix_begin = 0
+        for ix in range(ix_peak_begin, -1, -1):
+            if not exist_b[ix]:
+                ix_begin = ix
+                break
+        # insert slight margin before beginning
+        ix_begin = max(0, ix_begin - int(margin_beginning * freq))
+        ix_end = len(sound) - 1
+        for ix in range(ix_peak_end, len(sound)):
+            if not exist_e[ix]:
+                ix_end = ix
+                break
+        regions.append((ix_begin, ix_end))
+    # merge overlapping regions
+    if len(regions) == 0:
+        logger.warn("No collision region found")
+        return []
+    regions_merged = []
+    prev_region = regions[0]
+    for (i0, i1) in regions[1:]:
+        if i0 <= prev_region[1]:
+            # overlapping -> merge
+            prev_region = (
+                min(i0, prev_region[0]),
+                max(i1, prev_region[1]))
+        else:
+            # non-overlapping -> commit previous
+            regions_merged.append(prev_region)
+            prev_region = (i0, i1)
+    regions_merged.append(prev_region)
+    logger.info("%d regions found", len(regions_merged))
+
+    # visualize regions
+    reg_v = sound.copy() * 0
+    for (i0, i1) in regions_merged:
+        reg_v[i0:i1] = 0.5
+    import matplotlib.pyplot as plt
+    plt.plot(sound)
+    plt.plot(reg_v)
+    plt.show()
+
+    # clip and normalize each region
+    collisions = []
+    for (i0, i1) in regions_merged:
+        coll = sound[i0:i1]
+        collisions.append(coll / np.max(np.abs(coll)))
+    return collisions
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description="""
@@ -144,10 +232,15 @@ If a sound asset is stereo, only the first channel will be used.
         logger.info("writing soundscape to %s", args.simulate)
         write_wave_asset(args.simulate, samples)
     elif args.gen is not None:
-        for (i, e) in enumerate(os.listdir(args.sound_assets)):
+        sounds = []
+        for e in os.listdir(args.sound_assets):
             path = os.path.join(args.sound_assets, e)
             logger.info('Reading wav from %s', path)
             asset = read_wave_asset(path, freq=base_freq)
+            sounds += split_collisions(asset)
+
+        logger.info("%d total collisions found", len(sounds))
+        for (i, sound) in enumerate(sounds):
             write_wave_asset(
                 "%s/collision-%d.wav" % (args.gen, i + 1),
-                asset)
+                sound)
